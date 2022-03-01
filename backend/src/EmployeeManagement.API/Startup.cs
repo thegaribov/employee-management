001026978239ec197ccba90e7c7839b9475efc94
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-03-01 14:10:12 +0400

    Startup configured for data access layer

--- backend/src/EmployeeManagement.API/Startup.cs
+++ backend/src/EmployeeManagement.API/Startup.cs
@@ -1,6 +1,10 @@
+using EmployeeManagement.DataAccess.Persistance.Contexts;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,18 +18,90 @@ namespace EmployeeManagement.API
 {
     public class Startup
     {
+        public bool IsProduction { get; set; }
+        public bool IsStaging { get; set; }
+        public bool IsDevelopment { get; set; }
+        public IConfiguration Configuration { get; }
+
+        public Startup()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            IsProduction = environment == Environments.Production;
+            IsStaging = environment == Environments.Staging;
+            IsDevelopment = environment == Environments.Development;
+        }
+
         public Startup(IConfiguration configuration)
+            :this()
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
+
+            #region Database context
+
+            services.AddDbContext<EmployeeManagementContext>(option =>
+            {
+                var connectionString = string.Empty;
+
+                if (IsDevelopment)
+                {
+                    //Environment.GetEnvironmentVariable("CONNECTION_STRING_NAME")
+                    //Production
+
+                    connectionString = Configuration
+                           .GetConnectionString(Environment.GetEnvironmentVariable("CONNECTION_STRING_NAME"));
+                }
+                else
+                {
+                    string serverName = Environment.GetEnvironmentVariable("SQL_SERVER_NAME");
+                    string database = Environment.GetEnvironmentVariable("SQL_DATABASE");
+                    string user = Environment.GetEnvironmentVariable("SQL_USER");
+                    string password = Environment.GetEnvironmentVariable("SQL_PASSWORD");
+
+                    connectionString = @$"Server={serverName};Database={database};User={user};Password={password};";
+                }
+
+                option.UseSqlServer(connectionString, x => x.MigrationsAssembly("Data"));
+            });
+
+            #endregion
+
+            #region Routing configurations
+
+            //Lowercase Routing
+            services.AddRouting(options => options.LowercaseUrls = true);
+
+            #endregion
+
+            #region Services
+
+            //unitOfWork
+            //services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+           
+            #endregion
+
+
+            #region FluentValidation
+
+            services.AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<Startup>());
+
+            #endregion
+
+            #region Data protection keys
+
+            services.AddDataProtection()
+                .PersistKeysToDbContext<EmployeeManagementContext>();
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +115,7 @@ namespace EmployeeManagement.API
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
